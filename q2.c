bece942c9a088267cbb78f809a5178551b321a4c
YYT <yantianyin1999@gmail.com>
2026-04-06 15:03:04 -0400

    add run segment, now ready for testing

--- q2.c
+++ q2.c
@@ -39,19 +39,28 @@ char *gen_string(int n)
     char charset[] = {'0', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
                       'a', 'b', 'c', 'd', 'e', 'f', 'x', 'x', 'y'};
     int charset_size = 20;
-    char *str = malloc(n + 1); 
+    char *str = malloc(n + 1);
     for (int i = 0; i < n; i++)
         str[i] = charset[rand() % charset_size];
-    str[n] = '\0'; 
+    str[n] = '\0';
     return str;
 }
 
+// the run dfa with some segment of the string function
+void run_segment(char *str, int lo, int hi, int init_state, int *end_state, int *count)
+{
+    int state = init_state;
+    *count = 0;
+    for (int i = lo; i < hi; i++)
+        state = dfa_step(state, str[i], count);
+    *end_state = state;
+}
 
 int main(int argc, char *argv[])
 {
     int t = atoi(argv[1]); // number of optimistic threads
     int n = atoi(argv[2]); // string length
-    srand(42);                                                                                                                                           
+    srand(42);
     char *str = gen_string(n);
     printf("%s\n", str);
     return 0;
